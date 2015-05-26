b8df06df68af5188332d0d78a9d9e0be2edbe88a
Chris Sefkin <c.sefkin@gmail.com>
2015-05-26 12:48:35 -0700

    working

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,18 @@
 var triangleTracker = function(x,y,z) {
-  if ((x === y) && (y === z)) {
-    return "equilateral";
-  } else {
-    if ((x === y) && (x != z) || (x === z) && (x != y) || (y === z) && (y != x)) {
-      return "isosceles";
-  } else {
-    if ((x != y) && (y != z)) {
-      return "scalene";
-  } else {
-    return "This is not a triangle!"
+
+  if ((x + y <= z) || (x + z <= y) || (y + z <= x)) {
+      return "not a valid triangle";
   }
-    }
+
+  else if ((x === y) && (x != z) || (x === z) && (x != y) || (y === z) && (y != x)) {
+      return "isosceles";
+  }
+
+  else if ((x !== y) && (y !== z)) {
+      return "scalene";
+  }
+
+  else if ((x === y) && (y === z)) {
+    return "equilateral";
   }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -4,10 +4,14 @@ describe('triangleTracker', function() {
   });
 
   it("is isosceles for most triangles where two sides are equal", function() {
-    expect(triangleTracker(3,2,2)).to.equal("isosceles");
+    expect(triangleTracker(10,11,10)).to.equal("isosceles");
   });
 
   it("is scalene for most triangles where no sides are equal", function() {
-    expect(triangleTracker(3,2,1)).to.equal("scalene");
+    expect(triangleTracker(10,11,12)).to.equal("scalene");
+  });
+
+  it("is not a valid triangle if one side is the same or shorter than the other two combined", function() {
+    expect(triangleTracker(2, 2, 8)).to.equal("not a valid triangle");
   });
 });
